30ddc27e98119160bd27e66286050eab3149c299
ardiansyahraihan389-wq <ardiansyahraihan389@gmail.com>
2026-07-13 19:11:55 +0700

    Update database configuration in config.php

---
+++ bankend/config.php
@@ -0,0 +1,8 @@
+<?php
+// Ganti bagian di dalam tanda kutip dengan data Anda
+define('DB_HOST', 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com'); 
+define('DB_PORT', '4000'); 
+define('DB_NAME', 'db_tunestash'); 
+define('DB_USER', '3JEBCfUzvMmG7Ki.root'); 
+define('DB_PASS', 'JlEhDcXz0LUnsYRS'); 
+?>
